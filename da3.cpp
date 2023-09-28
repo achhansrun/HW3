4eafe23dc5b21d609013c75a2a28695d17628cb1
achhansrun <107165544+achhansrun@users.noreply.github.com>
2023-09-27 21:58:23 -0800

    final commit

--- da3.cpp
+++ da3.cpp
@@ -10,18 +10,43 @@
 #include <functional>
 using std::function;
 
-
+//Checks to find if the inputted function ff throws an exception.
 void didItThrow(const function<void()> & ff,
                 bool & threw)
 {
-    // TODO: WRITE THIS!!!
+    try
+    {
+          ff(); //function throws exception
+    }
+    catch (...)
+    {
+          threw = true; //found exception so setting threw to true
+          throw;
+    }
+    threw = false; //function doesn't error out so set threw to false
 }
 
-
-int gcd(int a,
-        int b)
+//Finds the greatest common denominator of two integers, a and b
+// pre: a != b, a and b both are greater then 0
+int gcd(int a, int b)
 {
-    return 42;  // DUMMY
-    // TODO: WRITE THIS!!!
+    //base case
+    if (a == 0)
+    {
+        return b;
+    }
+	else if (a == b)
+	{
+		return a;
+	}
+    //recursive case
+    if (a > b)
+    {
+		gcd(b, a);
+    }
+	else
+	{
+		gcd(b % a, a);
+	}
 }
 
--- da3.hpp
+++ da3.hpp
@@ -12,9 +12,9 @@
 #include "llnode.hpp"  // For LLNode
 #include <cstddef>     // For std::size_t
 #include <functional>  // For std::function
-#include <stdexcept>
 
-
+//Looks for the data of the given index in the linked list given
+//throws an std::out_of_range if the index is greater then the length of the linked list
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head,
 				 std::size_t index)
@@ -35,18 +35,31 @@ ValueType lookup(const LLNode<ValueType> * head,
 void didItThrow(const std::function<void()> & ff,
 				bool & threw);
 
-
+//Returns true if the given iterators' array is sorted
+//Pre: first != last
 template <typename FDIter>
-bool checkSorted(FDIter first,
-				 FDIter last)
+bool checkSorted(FDIter first, FDIter last)
 {
-	
+	if (first == last) {
+		return true;  // An empty range is always considered sorted.
+	}
+
+	FDIter next = first;
+	++next; // Advance to the next element.
+
+	while (next != last) {
+		if (*next < *first) {
+			return false;  // Found an out-of-order element.
+		}
+		++first;
+		++next;
+	}
+	return true;
 }
 
 
 // Implementation in source file
-int gcd(int a,
-		int b);
+int gcd(int a, int b);
 
 
 #endif  //#ifndef FILE_DA3_HPP_INCLUDED
