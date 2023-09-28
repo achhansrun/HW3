5af8ba75c7a4683fb501e74d6bad4533323d2f79
achhansrun <107165544+achhansrun@users.noreply.github.com>
2023-09-27 18:33:22 -0800

    Finished lookup function

--- da3.hpp
+++ da3.hpp
@@ -8,32 +8,25 @@
 #ifndef FILE_DA3_HPP_INCLUDED
 #define FILE_DA3_HPP_INCLUDED
 
+#include <iostream>
 #include "llnode.hpp"  // For LLNode
 #include <cstddef>     // For std::size_t
 #include <functional>  // For std::function
+#include <stdexcept>
 
 
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head,
 				 std::size_t index)
 {
-	for(int i = 0; i < index; i++)
+	for(int i = 0; i < index && head != nullptr; i++)
 	{
-		try
-		{
-		if(head != nullptr)
-		{
 		head = head->_next;
-		}
-		else
-		{
-			return 0;
-		}
-		} catch(std::out_of_range)
-		{
-			
-		}
-	} 
+	}
+	if (head == nullptr)
+	{
+		throw std::out_of_range("Index is out of range!");
+	}
 	return head->_data;
 }
 
@@ -47,8 +40,7 @@ template <typename FDIter>
 bool checkSorted(FDIter first,
 				 FDIter last)
 {
-	return false;  // DUMMY
-	// TODO: WRITE THIS!!!
+	
 }
 
 
